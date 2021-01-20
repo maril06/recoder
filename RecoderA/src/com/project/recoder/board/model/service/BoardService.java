09ab015b6bac62ef6f0a70ecefbfd93732602012
LEEJUNHO <LEEJUNHO@DESKTOP-R1EF7JO>
2021-01-20 17:38:00 +0900

    회원관리 업데이트

--- RecoderA/src/com/project/recoder/board/model/service/BoardService.java
+++ RecoderA/src/com/project/recoder/board/model/service/BoardService.java
@@ -3,6 +3,7 @@ package com.project.recoder.board.model.service;
 import static com.project.recoder.common.JDBCTemplate.*;
 
 
+
 import java.sql.Connection;
 import java.util.List;
 
--- RecoderA/src/com/project/recoder/member/model/service/MemberService.java
+++ RecoderA/src/com/project/recoder/member/model/service/MemberService.java
@@ -1,12 +1,46 @@
 package com.project.recoder.member.model.service;
 
+import static com.project.recoder.common.JDBCTemplate.*;
+
+import java.sql.Connection;
+import java.util.List;
+
+import com.project.recoder.member.model.dao.MemberDAO;
+import com.project.recoder.member.model.vo.Member;
 import com.project.recoder.member.model.vo.PageInfo;
 
 public class MemberService {
+	
+	private MemberDAO dao = new MemberDAO();
+	
+	
+	/** 전체회원 수 반환 Service
+	 * @param cp
+	 * @return
+	 * @throws Exception
+	 */
+	public PageInfo getPageInfo(String cp) throws Exception {
+		
+		Connection conn = getConnection();
+		
+		int currentPage = cp == null ? 1 : Integer.parseInt(cp);
+		
+		int listCount = dao.getListCount(conn);	
+		
+		close(conn);
+		
+		return new PageInfo(currentPage, listCount);
+	}
 
-	public PageInfo getPageInfo(String cp) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Member> selectMemberList(PageInfo pInfo) throws Exception{
+		
+		Connection conn = getConnection();
+		
+		List<Member> mList = dao.selectMemberList(conn, pInfo);
+		
+		close(conn);
+		
+		return mList;
 	}
 
 	
